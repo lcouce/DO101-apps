d815b74c18fdc1edd0abc58b0bd63cc33dce0400
Luis Alejandro Couce <LCOUCE@claro.com.ar>
2020-05-18 19:04:30 -0300

    Add mars feature

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello asdf!\n');
+});
+
+app.get('/Mars', function (req, res) {
+  res.send('Hello Mars!\n');
 });
 
 app.listen(8080, function () {
